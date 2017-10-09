aaae117d96f1193c7fa5d7b8a0291a627b3bf31e
F.S. Kroes <fskroes@hotmail.com>
2017-10-09 14:13:13 +0200

    Made fields + linked views to controllers

--- src/main/java/nl/webedu/hourregistration/controller/UserAuthenticationController.java
+++ src/main/java/nl/webedu/hourregistration/controller/UserAuthenticationController.java
@@ -1,5 +1,11 @@
 package nl.webedu.hourregistration.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class UserAuthenticationController {
 
+    public TextField emailText;
+    public TextField passwordText;
+    public Button signInButton;
 }
