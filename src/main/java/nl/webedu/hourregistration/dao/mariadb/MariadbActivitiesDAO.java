833255649dd45aecd8acdba185f71189b16b44d2
DenNie <Denzel_nievaart@hotmail.nl>
2017-10-11 11:12:25 +0200

    Pushing activitiesdao and databaseextension

--- src/main/java/nl/webedu/hourregistration/dao/mariadb/MariadbActivitiesDAO.java
+++ src/main/java/nl/webedu/hourregistration/dao/mariadb/MariadbActivitiesDAO.java
@@ -5,7 +5,12 @@ import nl.webedu.hourregistration.database.DatabaseManager;
 import nl.webedu.hourregistration.database.MariaDatabaseExtension;
 import nl.webedu.hourregistration.model.ActivitiesModel;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 
 public class MariadbActivitiesDAO implements IActivitiesDAO {
 
@@ -17,13 +22,60 @@ public class MariadbActivitiesDAO implements IActivitiesDAO {
 
     @Override
     public boolean insertActivitie(ActivitiesModel activitie) {
+
+        Connection dbConnection = null;
+        PreparedStatement ps = null;
+
+        String sql = "INSERT INTO activity"
+                + "(activityID, category, start_time, end_time, fk_workdayID) VALUES"
+                + "(?,?,?,?,?)";
+        try {
+            dbConnection = database.getConnection();
+            ps = database.getConnection().prepareStatement(sql);
+
+            ps.setInt(1, activitie.getId());
+            ps.setString(2, activitie.getCategory());
+            ps.setDate(3, (Date) activitie.getStartTime());
+            ps.setDate(4, (Date) activitie.getEndTime());
+            ps.setObject(5, activitie.getWorkday());
+
+            ps.executeUpdate();
+
+            System.out.println("Record toegevoegd");
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (ps != null) {
+                try {
+                    ps.getConnection().close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (dbConnection != null) {
+                try {
+                    dbConnection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return true;
-    }
+    };
 
     @Override
     public ActivitiesModel findActivitie(int id) {
 
-        return null;
+        ActivitiesModel activities = null;
+        try {
+            activities = database.selectObjectSingle(new ActivitiesModel(), "SELECT * FROM activities WHERE activityID = ?", id + "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return activities;
     }
 
     @Override
@@ -38,12 +90,25 @@ public class MariadbActivitiesDAO implements IActivitiesDAO {
     }
 
     @Override
-    public Collection selectActivitiesByWorkday(int wordkdatId) {
-        return null;
+    public Collection<ActivitiesModel> selectActivitiesByWorkday(int wordkdatId) {
+
+        List<ActivitiesModel> activities = null;
+        try {
+            activities = database.selectObjectList(new ActivitiesModel(), "SELECT * FROM activity WHERE activityID = ?", wordkdatId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return activities;
     }
 
     @Override
     public ActivitiesModel selectActivitiesByEmployee(int employeeId) {
-        return null;
+        ActivitiesModel activities = null;
+        try {
+            activities = database.selectObjectSingle(new ActivitiesModel(), "SELECT * FROM activity WHERE activityID = ?", employeeId + "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return activities;
     }
 }
