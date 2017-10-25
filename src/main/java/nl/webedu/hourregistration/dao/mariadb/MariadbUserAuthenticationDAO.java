986b99db0ec3a2254b2ac4bf1f6ca2a60738beac
Tristan Caspers <tristancaspers97@hotmail.com>
2017-10-25 12:03:28 +0200

    Added working sign in & register account on MariaDB

--- src/main/java/nl/webedu/hourregistration/dao/mariadb/MariadbUserAuthenticationDAO.java
+++ src/main/java/nl/webedu/hourregistration/dao/mariadb/MariadbUserAuthenticationDAO.java
@@ -3,11 +3,18 @@ package nl.webedu.hourregistration.dao.mariadb;
 import nl.webedu.hourregistration.dao.IUserAuthenticationDAO;
 import nl.webedu.hourregistration.database.DatabaseManager;
 import nl.webedu.hourregistration.database.MariaDatabaseExtension;
+import nl.webedu.hourregistration.model.ActivitiesModel;
+import nl.webedu.hourregistration.model.EmployeeModel;
 import nl.webedu.hourregistration.model.UserAuthenticationModel;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MariadbUserAuthenticationDAO implements IUserAuthenticationDAO {
 
     private static MariadbUserAuthenticationDAO instance;
+
+    private UserAuthenticationModel model;
     private MariaDatabaseExtension database;
 
     private MariadbUserAuthenticationDAO() {
@@ -23,16 +30,43 @@ public class MariadbUserAuthenticationDAO implements IUserAuthenticationDAO {
 
     @Override
     public UserAuthenticationModel findUser(String email) {
-        return null;
+        UserAuthenticationModel user = null;
+        String selectSQL = "SELECT * FROM employee WHERE email = ?";
+        try {
+            user = database.selectObjectSingle(new UserAuthenticationModel(), selectSQL, email);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
     @Override
     public void registerUser(String username, String password) {
+        model = null;
+        model = findUser(username);
+        if(model != null) {
+            System.out.println(username + " already exists");
+            return;
+        }
 
+        String insertSQL = "INSERT INTO employee"
+                + "(email, password) VALUES"
+                + "(?,?)";
+
+        try {
+            database.insertQuery(insertSQL, username, password);
+            System.out.println(username + " is registered");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public boolean authenticateUser(String email, String password) {
+        model = null;
+        model = findUser(email);
+        if(model != null)
+            return true;
         return false;
     }
 }
